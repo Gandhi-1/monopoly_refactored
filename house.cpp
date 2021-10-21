3018cd64ba8dff1a792bc6808b73a9a39af16ee8
Gandhi-1 <90591517+Gandhi-1@users.noreply.github.com>
2021-10-21 13:30:57 +1030

    fixed
    buying and selling

--- house.cpp
+++ house.cpp
@@ -13,13 +13,13 @@ House::House() {}
 // function for buying house
 virtual void House::buyHouse(Player p, Property pr) {
   p.change_balance(0.5*pr.price,'-');
-  pr.rent = pr.rent*1.1
-  num_house++;
+  pr.add_value(pr.rent*0.1);
+  pr.num_house++;
 }
 
 // function for selling house
 virtual void House::sellHouse(Player p, Property pr) {
   p.change_balance(0.5*pr.price,'+');
-  pr.rent = pr.rent/1.1;
-  num_house--;
+  pr.add_value(-1/11*pr.rent);
+  pr.num_house--;
 }
